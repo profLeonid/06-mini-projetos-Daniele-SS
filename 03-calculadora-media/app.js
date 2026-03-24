853d37711c6299e3bcb97c9693d56565fe7dc65a
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2026-03-24 07:59:52 -0300

    feat: calculadora de média com situação do aluno

--- 03-calculadora-media/app.js
+++ 03-calculadora-media/app.js
@@ -1,38 +1,35 @@
-'use scritc'
+'use strict'//Ativa o modo estrito do JS, que ajuda a capturar erros
 
-//Função para realizar o calculo da média artimetica
-const calcularMedia = (n1, n2, n3) => Number(n1) + Number(n2) + Number(n3) / 3
+const calcularMedia = (n1, n2, n3) => (Number(n1) + Number(n2) + Number(n3)) / 3
 
-//Função para validar se o aluno está: aprovado, de recuperação ou reprovado
-function situacaoDoAluno (nota) {
-
-    if ( nota >= 7) {
+function situacaoDoAluno(nota) {
+    if (nota >= 7) {
         return 'Aprovado'
-
-    } else if ( nota >= 5 || nota <= 6.9) {
+    } else if (nota >= 5 && nota <= 6.9) {
         return 'Recuperação'
-
-    } else if ( nota < 5) {
+    } else if (nota < 5) {
         return 'Reprovado'
     }
 }
 
 function cliqueButton() {
-    
+
     const n1        = Number(document.getElementById('n1').value)
     const n2        = Number(document.getElementById('n2').value)
     const n3        = Number(document.getElementById('n3').value)
     const resultado = document.getElementById('resultado')
-    
-    const status  = situacaoDoAluno()
 
-    
-    const media = calcularMedia (n1, n2, n3).toFixed(0)/*Chamo minha função de calculo e uso o .toFixed(0) 
-                                                        para que não apareça números após a vírgula*/
+    const media          = calcularMedia(n1, n2, n3)      
+    const mediaFormatada = media.toFixed(1)                    
+    const status         = situacaoDoAluno(media)              
 
-                                                        
-    //Exibição do resultado que o usuário irá visualizar na tela
-    resultado.textContent = `A média do aluno é ${media}, ele está ${status}`
-    resultado.classList.remove('status-aprovado', 'status-recuperacao', 'status-reprovado')
-    //resultado.classList.add(cor)
-}
+    const classeMap = {
+        'Aprovado':    'status-aprovado',
+        'Recuperação': 'status-recuperacao',
+        'Reprovado':   'status-reprovado'
+    }
+    //Exibe a média formatada e o status do aluno; se em recuperação, adiciona o complemento 'de'
+    resultado.textContent = `A média do(a) aluno(a) é ${mediaFormatada}, ele(a) está ${status === 'Recuperação' ? 'de Recuperação' : status}`
+    resultado.classList.remove('status-aprovado', 'status-recuperacao', 'status-reprovado') 
+    resultado.classList.add(classeMap[status])                                                  
+}                                                                                               
